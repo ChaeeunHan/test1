e63e27e107da298342cab9d84a9cb9b3e847385f
stlts7 <xkq1998@gmail.com>
2020-09-10 19:52:15 +0900

    feat: fix bug

--- calculator.py
+++ calculator.py
@@ -6,6 +6,9 @@ class calculator:
     def subtract(self, a, b):
         print('a - b =', a-b)
 
+    def multiply(self, a, b):
+    	print('a * b = ', a*b)
+
 def main():
     print('main')
 =======
